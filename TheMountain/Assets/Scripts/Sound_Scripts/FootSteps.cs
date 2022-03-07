97b0e70b03f31752459c49400afa1286c6e3fd82
Luke Collier <lukebcollier@gmail.com>
2022-03-06 17:27:01 -0800

    Fixed Footstep sound

--- TheMountain/Assets/Scripts/Sound_Scripts/FootSteps.cs
+++ TheMountain/Assets/Scripts/Sound_Scripts/FootSteps.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class FootSteps : MonoBehaviour
 {
-
+    public CharacterController2D CC2D;
+    public float time1;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 )
         {
-            FindObjectOfType<AudioMgr>().Play("FootSteps");
-            Debug.Log("FOOTSTEPS");
+
+            time1 += Time.deltaTime;
+            
+            if (time1 > 0.2 && CC2D.m_Grounded && Input.GetKey(KeyCode.LeftShift))
+            {
+                FindObjectOfType<AudioMgr>().Play("FootSteps");
+                time1 = 0;
+            }
+            else if (time1 > 0.4 && CC2D.m_Grounded)
+            {
+                FindObjectOfType<AudioMgr>().Play("FootSteps");
+                time1 = 0;
+            }
         }
     }
 }
