9c55ffbf73ad354326bac8b2ccdf0197feee27ea
Agui Navarro <aguinaldon@nevada.unr.edu>
2022-04-10 15:24:01 -0700

    Reverted to Conner PlayerHealth
    Will edit on AguiDev

--- TheMountain/Assets/Scripts/Player_Scripts/PlayerHealth.cs
+++ TheMountain/Assets/Scripts/Player_Scripts/PlayerHealth.cs
@@ -3,12 +3,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] CapsuleCollider2D Player;
-    [SerializeField] public GameObject blackImage;
     // max health and lives initiations
     public int maxHealth = 100;
     public int maxLives = 4;
@@ -18,10 +15,6 @@ public class PlayerHealth : MonoBehaviour
 
     private bool dead = false;
 
-    public int currentLives;
-    private bool blackOut;
-    private bool gameOverTrigger;
-    private bool reset;
     public static PlayerHealth inst;
 
     private Animator animation;
@@ -36,48 +29,6 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth = maxHealth;
         currentLives = maxLives;
-
-        blackOut = false;
-        reset = false;
-    }
-    void Update()
-    {
-        if (currentLives == 0)
-        {
-            if (!blackImage.activeSelf)
-            {
-                Color temp = blackImage.GetComponent<Image>().color;
-                temp = new Color(temp.r, temp.g, temp.b, 0.0f);
-                blackImage.GetComponent<Image>().color = temp;
-                blackImage.SetActive(true);
-            }
-            gameOverTrigger = true;
-            blackImage.SetActive(true);
-            StartCoroutine(FadeInAndOut(true, 1f));
-        }
-
-        Color checkAlpha = blackImage.GetComponent<Image>().color;
-        if (checkAlpha.a >= 1.0f && gameOverTrigger)
-        {
-            blackOut = true;
-        }
-
-        if (blackOut)
-        {
-            Debug.Log("Teleporting to spawn");
-            Player.transform.position = new Vector3(0, 0, 3);
-            blackOut = false;
-            gameOverTrigger = false;
-            currentLives = maxLives;
-            Debug.Log("All lives reset.");
-            Invoke("Wait", 1.5f);
-        }
-
-        if (checkAlpha.a <= 0.0f && reset)
-        {
-            blackImage.SetActive(false);
-            reset = false;
-        }
     }
 
     // if the player collides with an object with the enemy script included in it,
@@ -110,11 +61,6 @@ public class PlayerHealth : MonoBehaviour
             // current lives subtracted by one
             //currentLives--;
             GetComponent<LifeCount>().LoseLife();
-            currentLives--;
-            //GetComponent<LifeCount>().LoseLife();
-            // die function runs
-            Die();
-            currentHealth = 100;
         }
 
         // When the player is dead
@@ -136,6 +82,7 @@ public class PlayerHealth : MonoBehaviour
 
     
     }
+
     void Die()
     {
         Debug.Log("Player died");
