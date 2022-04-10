acc869115cdef2600f61045e190c4b76281df89a
Agui Navarro <aguinaldon@nevada.unr.edu>
2022-04-10 16:34:54 -0700

    Added button support for dialogue boxes
    Press return to click "help" or "continue"

--- TheMountain/Assets/Scripts/Dialogue_Scripts/DialogueTrigger.cs
+++ TheMountain/Assets/Scripts/Dialogue_Scripts/DialogueTrigger.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    public Button button;
     public Dialogue dialogue;
-
+    void Update() 
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            button.onClick.Invoke();
+        }
+    }
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
